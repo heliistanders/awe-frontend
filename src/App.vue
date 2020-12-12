e7d9b3886fe7c07d199b4092eda6686a7f0de4c7
Heli <helmuth.weithaler@gmail.com>
2020-12-12 13:07:36 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav>
+        <a href="/"><img src="/img/logo.svg" alt="Home" id="logo"></a>
+        <div class="nav_right">
+
+            <div class="lang_block">
+                <span class="lang_first">DE</span> / <span class="lang_second">EN</span>
+            </div>
+            <button class="search_block block">
+                <i class=" icon-ic_fluent_search_24_regular" id="search"></i>
+                <div>suche</div>
+            </button>
+            <button class="settings_block block">
+                <i class=" icon-ic_fluent_settings_24_regular" id="setting"></i>
+                <div>Settings</div>
+            </button>
+        </div>
+    </nav>
   <router-view/>
 </template>
 
@@ -15,13 +28,107 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+nav {
+  height: 80px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    flex-grow: 0;
+    padding: 0 80px;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, .4);
+    z-index: 100;
 }
 
-#nav a {
-  font-weight: bold;
+.nav_right button {
+  border: none;
+  background: none;
+  margin: 0;
+  padding: 0;
   color: #2c3e50;
+}
+
+.nav_right button:active, .nav_right button:checked {
+  border: none;
+}
+
+.nav_right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#search, #setting {
+    font-size: 24px;
+}
+
+.block:hover {
+  font-size: 10px;
+}
+
+.search_block {
+    width: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 9px;
+    color: rgba(0, 0, 0, .64);
+    text-transform: uppercase;
+}
+
+.lang_block {
+    width: 80px;
+    font-size: 13px;
+    text-align: center;
+    vertical-align: bottom;
+}
+
+.lang_block:hover {
+  font-size: 14px;
+}
+
+.lang_first {
+    color: rgba(0, 0, 0, 1);
+}
+
+.lang_first:hover, .lang_second:hover {
+    color: #76b82a;
+}
+
+.lang_second {
+    color: rgba(0, 0, 0, .56);
+}
+
+.settings_block {
+    width: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 9px;
+    color: rgba(0, 0, 0, .64);
+    text-transform: uppercase;
+}
+
+nav a {
+    width: 134px;
+}
+
+#logo {
+    height: 34px;
+}
+
+#nav img {
+  height: 90%;
+  width: auto;
+}
+
+
+#nav a:hover {
+  background: black;
 }
 
 #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,104 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table class="m_table">
+        <thead class="m_header">
+            <tr class="m_header_row">
+                <th class="m_header_name">Name</th>
+                <th class="m_header_difficulty">Difficulty</th>
+                <th class="m_header_owned">Owned</th>
+                <th class="m_header_status">Status</th>
+                <th class="m_header_actions">Actions</th>
+            </tr>
+        </thead>
+        <tbody class="m_body">
+            <tr class="m_row" v-for="m in machines" :key="m.id">
+                <td class="m_name">{{m.name}}</td>
+                <td class="m_difficulty">{{m.difficulty}}</td>
+
+                <td v-if="m.owned" class="m_owned"><i class=" icon-ic_fluent_checkmark_circle_24_regular" id="search"></i></td>
+                <td v-else class="m_owned"><i class=" icon-ic_fluent_dismiss_circle_24_regular" id="search"></i></td>
+                <td class="m_status"><span>{{m.status}}</span></td>
+                <td class="m_actions">
+                    <a v-if="m.status == 'stopped'" href="/start/{{m.id}}">
+                        <i class=" icon-ic_fluent_play_circle_24_regular"></i>
+                    </a>
+                    <span v-else>
+                      <a href="/restart/{{m.id}}">
+                          <i class=" icon-ic_fluent_arrow_sync_circle_24_regular"></i>
+                      </a>
+                      <a href="/stop/{{m.id}}">
+                          <i class=" icon-ic_fluent_dismiss_circle_24_regular"></i>
+                      </a>
+                    </span>
+                    <a href="/hint/{{m.id}}">
+                        <i class=" icon-ic_fluent_lightbulb_circle_24_regular"></i>
+                    </a>
+                    <a href="/help/{{m.id}}">
+                        <i class=" icon-ic_fluent_question_circle_24_regular"></i>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      machines: [
+        {"id": 1,"name": "Deserialization", "difficulty": "Easy", "owned": true, "status": "stopped"},
+        {"id": 2,"name": "Server Side Template Injection", "difficulty": "Hard", "owned": false, "status": "running"},
+      ]
+    }
   }
 }
 </script>
+
+
+<style>
+.home {
+  padding: 40px 100px;
+}
+
+.m_table {
+  width: 100%;
+  text-align: center;
+  border-collapse: collapse;
+}
+
+.m_header {
+    border-bottom: 2px solid black;
+}
+
+.m_row {
+    height: 40px;
+}
+
+.m_header_name, .m_name {
+    text-align: left;
+}
+
+.m_owned {
+    vertical-align: bottom;
+}
+
+.m_actions {
+    vertical-align: bottom;
+    font-size: 24px;
+}
+
+.m_actions a,a:visited,a:hover,a:link {
+    color: black;
+    text-decoration: none;
+}
+
+i:hover {
+  color: red;
+}
+</style>
